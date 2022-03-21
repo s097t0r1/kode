cdc285984e5ccefdfaf875957870cb7ed84cd604
s097t0r1 <zmitr241@yandex.ru>
2022-03-21 15:09:00 +0300

    feat(app.ui.main): add error screen to main screen

--- app/src/main/java/com/s097t0r1/kode/ui/main/MainActivity.kt
+++ app/src/main/java/com/s097t0r1/kode/ui/main/MainActivity.kt
@@ -3,26 +3,42 @@ package com.s097t0r1.kode.ui.main
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
+import androidx.activity.viewModels
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.s097t0r1.domain.entities.Department
+import com.s097t0r1.kode.R
 import com.s097t0r1.kode.ui.theme.KodeTheme
 
 class MainActivity : ComponentActivity() {
+
+    val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             KodeTheme {
                 Scaffold {
-                    MainScreen(Modifier.padding(it), {}, {})
+                    MainErrorScreen(
+                        modifier = Modifier
+                            .padding(it)
+                            .fillMaxSize(),
+                        onRetryClick = { }
+                    )
                 }
-
             }
         }
     }
@@ -44,6 +60,41 @@ fun MainScreen(
         )
         DepartmentTabs(onTabClick)
     }
+}
+
+@Composable
+fun MainErrorScreen(
+    modifier: Modifier = Modifier,
+    onRetryClick: () -> Unit
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            modifier = Modifier.size(56.dp),
+            painter = painterResource(R.drawable.ic_flying_saucer),
+            contentDescription = null
+        )
+        Text(
+            text = stringResource(R.string.critical_error_title),
+            style = MaterialTheme.typography.subtitle1
+        )
+        Text(
+            text = stringResource(R.string.critical_error_subtitle),
+            style = MaterialTheme.typography.caption
+        )
+        TextButton(onClick = onRetryClick) {
+            Text(text = stringResource(R.string.critical_error_retry))
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainErrorScreenPreview() {
+    MainErrorScreen(onRetryClick = {})
 }
 
 @Preview(showBackground = true)
