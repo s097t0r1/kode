95f41070a861e44428219be7fb5f4370e3b342f7
s097t0r1 <zmitr241@yandex.ru>
2022-03-28 15:56:33 +0300

    feat(data): add parcelable version of User domain model

---
+++ data/src/main/java/com/s097t0r1/data/model/DataUser.kt
@@ -0,0 +1,40 @@
+package com.s097t0r1.data.model
+
+import android.os.Parcelable
+import com.s097t0r1.domain.entities.User
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class DataUser(
+    val id: String,
+
+    val avatarUrl: String,
+
+    val firstName: String,
+
+    val lastName: String,
+
+    val userTag: String,
+
+    val department: String,
+
+    val position: String,
+
+    val birthday: Long,
+
+    val phone: String
+
+) : Parcelable
+
+fun User.toDataModel() =
+    DataUser(
+        id = this.id,
+        avatarUrl = this.avatarUrl,
+        firstName = this.firstName,
+        lastName = this.lastName,
+        userTag = this.userTag,
+        department = this.department.name,
+        position = this.position,
+        birthday = this.birthday.time,
+        phone = this.phone
+    )
